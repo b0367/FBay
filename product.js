354791727b633151c0e09d85ea7bb3361b18b896
Glitch (fbay) <none>
2019-07-20 22:35:29 +0000

    🎬🏀 Updated with Glitch

--- product.js
+++ product.js
@@ -1,1 +1,10 @@
-$.getJSON("", function(data) {});
+var urlParams = new URLSearchParams(window.location.search);
+
+
+$.getJSON("https://fbay-server.herokuapp.com/items/?id=" + urlParams.get("id"), (data)=>{
+  var realjson = JSON.parse(data.message);
+  document.getElementById("name").innerText = realjson.name;
+  document.getElementById("description").innerText = realjson.description;
+  document.getElementById("imgurl").src = realjson.imageurl;
+  document.getElementById("price").innerText = "$" + realjson.price;
+});
