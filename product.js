4ce98b9d0f72971d013c019193d733180a9d9cca
Glitch (fbay) <none>
2019-07-21 08:40:15 +0000

    🕰🚂 Updated with Glitch

--- product.js
+++ product.js
@@ -7,7 +7,10 @@ $.getJSON("https://fbay-server.herokuapp.com/items/?id=" + urlParams.get("id"), 
   document.getElementById("description").innerText = realjson.description;
   document.getElementById("imgurl").src = realjson.imageurl;
   document.getElementById("price").innerHTML = "List price: <b>$" + realjson.price + "</b>";
-  document.getElementById("current").innerHTML = "Now: <b>$" + (realjson.price + ((Math.floor(Math.random() * 25) + 25) * realjson.increment)) + "<b/>";
+  let value = (realjson.price + ((Math.floor(Math.random() * 25) + 25) * realjson.increment));
+  document.getElementById("current").innerHTML = "Now: <b>$" + value + "<b/>";
+  document.getElementById("rawcurrent").innerText = value;
+  document.getElementById("increment").innerText = realjson.increment;
 });
 
 function timer(){
@@ -31,6 +34,35 @@ timer();
 
 function SubmitBid(){
   var urlParams = new URLSearchParams(window.location.search);
-  let increment = document.getElementById("increment").value;
-  $.getJSON("")
+  let increment = document.getElementById("increment").innerText;
+  let current = document.getElementById("rawcurrent").innerText;
+  $.getJSON("https://fbay-server.herokuapp.com/bids/query?" + urlParams.get("id"), function(data){
+    if(data.length >= 3){
+       current = data.amount;
+      document.getElementById("current").innerHTML = "Now <b>$" + current + "</b>";
+    }
+    $.getJSON("https://fbay-server.herokuapp.com/bids/create?username=" + getCookie("username").split("=")[1] + "&item=" + urlParams.get("id") + "&amount=" + (increment + current), function(data){
+        if(data.message === "success"){
+          alert("you placed a bid on the item!");
+          document.getElementById("current").innerHTML = "Now <b>$" + current + "</b>";
+        }else{
+          alert("something went wrong, and the bid couldn't be placed.");
+        }
+      });
+  });
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.value);
+    }
+  }
+  return "";
 }
